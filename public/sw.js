6984af8a9a27f15211f33238ccc07d9ae1f064eb
Yuvraj <yuvraj15082007@gmail.com>
2026-04-14 23:43:39 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE = "fittrack-v2";
+const ASSETS = ["/", "/index.html", "/static/js/main.chunk.js", "/static/js/bundle.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
